aa8ced23696a45345980cac5116008b19af2c048
sampathdissanayaka <dmcsampath97@gmail.com>
2021-01-03 00:08:54 +0700

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -187,9 +187,58 @@ class _MyHomePageState extends State<MyHomePage> {
                               borderRadius: BorderRadius.all(Radius.circular(30)),
                               color: green,
                                 boxShadow: [BoxShadow(spreadRadius: 0, offset: Offset(0,10), blurRadius: 0, color: green.withOpacity(0.5))]
+                            ),
+                              child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: AssetImage("asset/images/food1.png"),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 15,),
+                                Text("Yoshimasa Sushi", style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                                ),
+                                SizedBox(height: 5,),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.white,
+                                      size: 17,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.white,
+                                      size: 17,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.white,
+                                      size: 17,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.white,
+                                      size: 17,
+                                    ),
+
+                                  ],
+                                ),
+
+
+                              ],
+                            ) ,
 
                             ),
-                          ),
                         ],
                       ),
                     ],
